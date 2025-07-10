01db7393bda501790b0af5c736498cc69937e601
Sunanda01 <sunandasadhukhan1@gmail.com>
2025-07-11 01:09:22 +0530

    Created Custom useEffect Hook using useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import CustomAPIFetch from "./views/custom-api-fetch";
 import CustomDrawer from "./views/custom-drawer";
+import UseCustomEffect from "./views/custom-useEffect-hook";
 
 function App() {
   return (
     <>
       {/* <h1>Machine Coding</h1> */}
       {/* <CustomDrawer/> */}
-      <CustomAPIFetch />
+      {/* <CustomAPIFetch /> */}
+      <UseCustomEffect/>
     </>
   );
 }
